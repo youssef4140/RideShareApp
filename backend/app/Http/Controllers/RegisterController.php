80c1cd984d87d5d86d9d4e7b7581a7b61152a433
youssef4140 <youssef.mostafa4140@gmail.com>
2024-02-29 04:20:19 +0200

    added register controller

---
+++ backend/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request):JsonResponse
+    {
+
+    }
+}
