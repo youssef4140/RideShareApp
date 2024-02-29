630fa7ab6b842611568099c5391bf5a150208540
youssef4140 <youssef.mostafa4140@gmail.com>
2024-02-29 04:19:19 +0200

    created models and migrations

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'phone',
         'password',
     ];
 
@@ -42,4 +42,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function trip()
+    {
+        return $this->hasMany(Trip::class);
+    }
+
+    public function driver()
+    {
+        return $this->hasOne(Driver::class);
+    }
 }
--- backend/database/migrations/2024_02_29_015614_create_drivers_table.php
+++ backend/database/migrations/2024_02_29_015614_create_drivers_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,14 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('drivers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('phone')->unique();
-            $table->string('password');
-            $table->boolean('isVerified')->default(false);
-            $table->boolean('isDriver')->default(false);
-            $table->rememberToken();
+            $table->foreignIdFor(User::class);
+            $table->integer('year');
+            $table->string('model');
+            $table->string('color');
+            $table->string('license_plate');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('drivers');
     }
 };
